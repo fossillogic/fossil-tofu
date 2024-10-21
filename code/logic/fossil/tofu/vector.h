969b6e96edc92c77a4e1def52addb215d5f8f446
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2024-10-21 12:10:29 -0500

    add new functions to vector

--- code/logic/fossil/tofu/vector.h
+++ code/logic/fossil/tofu/vector.h
@@ -60,6 +60,27 @@ void fossil_vector_destroy(fossil_vector_t* vector);
 void fossil_vector_push_back(fossil_vector_t* vector, fossil_tofu_t element);
 
 /**
+ * Add an element to the front of the vector.
+ * 
+ * Time complexity: O(n)
+ *
+ * @param vector  The vector to which the element will be added.
+ * @param element The element to add.
+ */
+void fossil_vector_push_front(fossil_vector_t* vector, fossil_tofu_t element);
+
+/**
+ * Add an element at the specified index in the vector.
+ * 
+ * Time complexity: O(n)
+ *
+ * @param vector  The vector to which the element will be added.
+ * @param index   The index at which to add the element.
+ * @param element The element to add.
+ */
+void fossil_vector_push_at(fossil_vector_t* vector, size_t index, fossil_tofu_t element);
+
+/**
  * Remove the last element from the vector.
  * 
  * Time complexity: O(1)
@@ -67,6 +88,45 @@ void fossil_vector_push_back(fossil_vector_t* vector, fossil_tofu_t element);
  * @param vector The vector from which to remove the last element.
  */
 void fossil_vector_pop_back(fossil_vector_t* vector);
+
+/**
+ * Remove the first element from the vector.
+ * 
+ * Time complexity: O(n)
+ *
+ * @param vector The vector from which to remove the first element.
+ */
+void fossil_vector_pop_front(fossil_vector_t* vector);
+
+/**
+ * Remove the element at the specified index in the vector.
+ * 
+ * Time complexity: O(n)
+ *
+ * @param vector The vector from which to remove the element.
+ * @param index  The index at which to remove the element.
+ */
+void fossil_vector_pop_at(fossil_vector_t* vector, size_t index);
+
+/**
+ * Remove all elements from the vector.
+ * 
+ * Time complexity: O(1)
+ *
+ * @param vector The vector from which to remove all elements.
+ */
+void fossil_vector_erase(fossil_vector_t* vector, size_t index);
+
+/**
+ * Remove all elements from the vector that match the target element.
+ * 
+ * Time complexity: O(n)
+ *
+ * @param vector The vector from which to remove elements.
+ * @param target The element to remove.
+ */
+void fossil_vector_erase_if(fossil_vector_t* vector, fossil_tofu_t target);
+
 
 /**
  * Search for a target element in the vector.
@@ -161,6 +221,16 @@ fossil_tofu_t* fossil_vector_getter(const fossil_vector_t* vector, size_t index)
 size_t fossil_vector_size(const fossil_vector_t* vector);
 
 /**
+ * Get the capacity of the vector.
+ * 
+ * Time complexity: O(1)
+ *
+ * @param vector The vector for which to get the capacity.
+ * @return       The capacity of the vector.
+ */
+size_t fossil_vector_capacity(const fossil_vector_t* vector);
+
+/**
  * Display the contents of the vector.
  * 
  * Time complexity: O(n)
@@ -188,6 +258,26 @@ namespace fossil {
 
         void push_back(fossil_tofu_t element) {
             fossil_vector_push_back(vector_, element);
+        }
+
+        void push_front(fossil_tofu_t element) {
+            fossil_vector_push_front(vector_, element);
+        }
+
+        void push_at(size_t index, fossil_tofu_t element) {
+            fossil_vector_push_at(vector_, index, element);
+        }
+
+        void pop_back() {
+            fossil_vector_pop_back(vector_);
+        }
+
+        void pop_front() {
+            fossil_vector_pop_front(vector_);
+        }
+
+        void pop_at(size_t index) {
+            fossil_vector_pop_at(vector_, index);
         }
 
         int search(fossil_tofu_t target) {
@@ -224,6 +314,10 @@ namespace fossil {
 
         size_t size() {
             return fossil_vector_size(vector_);
+        }
+
+        size_t capacity() {
+            return fossil_vector_capacity(vector_);
         }
 
         void peek() {
