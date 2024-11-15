84a220502ecc9ec7c2a98fd5b9c6e7c00780f64f
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2024-11-15 17:42:16 -0600

    change

--- code/tests/tools/generate-runner.py
+++ code/tests/tools/generate-runner.py
@@ -9,7 +9,6 @@ class TestRunnerGenerator:
 
     def find_test_groups(self):
         c_test_groups = set()
-        cpp_test_groups = set()
         pattern = r"FOSSIL_TEST_GROUP\((\w+)\)"
 
         # Walk through files in the specified directory, 'cases'
@@ -21,14 +20,8 @@ class TestRunnerGenerator:
                         content = f.read()
                         matches = re.findall(pattern, content)
                         c_test_groups.update(matches)
-                # Search for C++ files
-                elif file.startswith("test_") and file.endswith(".cpp"):
-                    with open(os.path.join(root, file), "r") as f:
-                        content = f.read()
-                        matches = re.findall(pattern, content)
-                        cpp_test_groups.update(matches)
 
-        return list(c_test_groups), list(cpp_test_groups)
+        return list(c_test_groups)
 
     def generate_c_runner(self, c_test_groups):
         # Prepare header content for C test runner
@@ -67,7 +60,7 @@ int main(int argc, char **argv) {
 } // end of func
 """
 
-        # Write the generated C test runner to 'unit_runner_c.cpp'
+        # Write the generated C test runner to 'unit_runner.c'
         with open("unit_runner.c", "w") as file:
             file.write(header)
             file.write(extern_c_pools)
@@ -75,56 +68,10 @@ int main(int argc, char **argv) {
             file.write(import_c_pools)
             file.write(footer)
 
-    def generate_cpp_runner(self, cpp_test_groups):
-        # Prepare header content for C++ test runner
-        header = """
-// Generated Fossil Logic Test (C++)
-#include <fossil/test/framework.h>
 
-// * * * * * * * * * * * * * * * * * * * * * * * *
-// * Fossil Logic Test List (C++)
-// * * * * * * * * * * * * * * * * * * * * * * * *
-"""
-
-        # Declare C++ test group externs
-        extern_cpp_pools = "\n".join(
-            [f"FOSSIL_TEST_EXPORT({group});" for group in cpp_test_groups]
-        )
-
-        # Prepare runner content for C++
-        runner = """\n
-// * * * * * * * * * * * * * * * * * * * * * * * *
-// * Fossil Logic Test Runner (C++)
-// * * * * * * * * * * * * * * * * * * * * * * * *
-int main(int argc, char **argv) {
-    FOSSIL_TEST_START(argc, argv);\n"""
-
-        # Import C++ test groups in the main function
-        import_cpp_pools = "\n".join(
-            [f"    FOSSIL_TEST_IMPORT({group});" for group in cpp_test_groups]
-        )
-
-        # Complete with footer
-        footer = """\n
-    FOSSIL_TEST_RUN();
-    FOSSIL_TEST_SUMMARY();
-    FOSSIL_TEST_END();
-} // end of func
-"""
-
-        # Write the generated C++ test runner to 'unit_runner_cpp.cpp'
-        with open("unit_runner.cpp", "w") as file:
-            file.write(header)
-            file.write(extern_cpp_pools)
-            file.write(runner)
-            file.write(import_cpp_pools)
-            file.write(footer)
-
-
-# Instantiate the generator, find test groups, and generate the test runners
+# Instantiate the generator, find test groups, and generate the test runner for C
 generator = TestRunnerGenerator()
-c_test_groups, cpp_test_groups = generator.find_test_groups()
+c_test_groups = generator.find_test_groups()
 
-# Generate separate runners for C and C++
+# Generate the C test runner
 generator.generate_c_runner(c_test_groups)
-generator.generate_cpp_runner(cpp_test_groups)
