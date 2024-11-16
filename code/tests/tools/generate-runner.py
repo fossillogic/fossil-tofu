c87515d67e2d358e88c99b227aa912f71d67205b
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2024-11-16 11:55:24 -0600

    update python script

--- code/tests/tools/generate-runner.py
+++ code/tests/tools/generate-runner.py
@@ -8,48 +8,48 @@ class TestRunnerGenerator:
         self.directory = os.path.join(os.getcwd(), "cases")
 
     def find_test_groups(self):
-        c_test_groups = set()
+        test_groups = set()
         pattern = r"FOSSIL_TEST_GROUP\((\w+)\)"
 
         # Walk through files in the specified directory, 'cases'
         for root, _, files in os.walk(self.directory):
             for file in files:
-                # Search for C files
-                if file.startswith("test_") and file.endswith(".c"):
+                # Search for C and C++ files
+                if (file.startswith("test_") and file.endswith(".c")) or file.endswith(".cpp"):
                     with open(os.path.join(root, file), "r") as f:
                         content = f.read()
                         matches = re.findall(pattern, content)
-                        c_test_groups.update(matches)
+                        test_groups.update(matches)
 
-        return list(c_test_groups)
+        return list(test_groups)
 
-    def generate_c_runner(self, c_test_groups):
-        # Prepare header content for C test runner
+    def generate_c_runner(self, test_groups):
+        # Prepare header content for the test runner
         header = """
-// Generated Fossil Logic Test (C)
+// Generated Fossil Logic Test Runner
 #include <fossil/test/framework.h>
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
-// * Fossil Logic Test List (C)
+// * Fossil Logic Test List
 // * * * * * * * * * * * * * * * * * * * * * * * *
 """
 
-        # Declare C test group externs within extern "C"
-        extern_c_pools = "\n".join(
-            [f"FOSSIL_TEST_EXPORT({group});" for group in c_test_groups]
+        # Declare test group externs
+        extern_test_groups = "\n".join(
+            [f"FOSSIL_TEST_EXPORT({group});" for group in test_groups]
         )
 
-        # Prepare runner content for C
+        # Prepare runner content
         runner = """\n
 // * * * * * * * * * * * * * * * * * * * * * * * *
-// * Fossil Logic Test Runner (C)
+// * Fossil Logic Test Runner
 // * * * * * * * * * * * * * * * * * * * * * * * *
 int main(int argc, char **argv) {
     FOSSIL_TEST_START(argc, argv);\n"""
 
-        # Import C test groups in the main function
-        import_c_pools = "\n".join(
-            [f"    FOSSIL_TEST_IMPORT({group});" for group in c_test_groups]
+        # Import test groups in the main function
+        import_test_groups = "\n".join(
+            [f"    FOSSIL_TEST_IMPORT({group});" for group in test_groups]
         )
 
         # Complete with footer
@@ -57,21 +57,21 @@ int main(int argc, char **argv) {
     FOSSIL_TEST_RUN();
     FOSSIL_TEST_SUMMARY();
     FOSSIL_TEST_END();
-} // end of func
+} // end of main
 """
 
-        # Write the generated C test runner to 'unit_runner.c'
+        # Write the generated test runner to 'unit_runner.c'
         with open("unit_runner.c", "w") as file:
             file.write(header)
-            file.write(extern_c_pools)
+            file.write(extern_test_groups)
             file.write(runner)
-            file.write(import_c_pools)
+            file.write(import_test_groups)
             file.write(footer)
 
 
-# Instantiate the generator, find test groups, and generate the test runner for C
+# Instantiate the generator, find test groups, and generate the test runner
 generator = TestRunnerGenerator()
-c_test_groups = generator.find_test_groups()
+test_groups = generator.find_test_groups()
 
-# Generate the C test runner
-generator.generate_c_runner(c_test_groups)
+# Generate the test runner for C and C++ tests
+generator.generate_c_runner(test_groups)
