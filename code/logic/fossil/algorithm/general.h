00f1ef232dd66e15518c3aac3ea1aa7160b47392
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-03-24 21:43:56 -0600

    headers for algorithms

---
+++ code/logic/fossil/algorithm/general.h
@@ -0,0 +1,39 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ *
+ * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#ifndef FOSSIL_ALGO_GENERAL_H
+#define FOSSIL_ALGO_GENERAL_H
+
+#include "tofu.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+//
+
+#ifdef __cplusplus
+}
+
+namespace fossil {
+
+namespace tofu {
+
+} // namespace tofu
+
+} // namespace fossil
+
+#endif
+
+#endif /* FOSSIL_TOFU_FRAMEWORK_H */
